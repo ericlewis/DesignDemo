8c80a076bc938517f615cee67ceeba1069dd15a3
Eric Lewis <ericlewis777@gmail.com>
2014-06-06 10:03:53 -0700

    inital commit

---
+++ DesignDemo/EELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EELAppDelegate.h
+//  DesignDemo
+//
+//  Created by 1debit on 6/6/14.
+//  Copyright (c) 2014 1debit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DesignDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DesignDemo
+//
+//  Created by 1debit on 6/6/14.
+//  Copyright (c) 2014 1debit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EELAppDelegate class]));
+    }
+}
